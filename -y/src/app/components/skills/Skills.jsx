8d831a6735fb4d2e9a2d93d1704e4e36fbb06d74
mghazidev <mghazidev@gmail.com>
2023-09-12 12:56:56 +0500

    my-website

--- -y/src/app/components/skills/Skills.jsx
+++ -y/src/app/components/skills/Skills.jsx
@@ -6,37 +6,47 @@ const Skills = () => {
     <div className="text-center bg-white py-16">
       <div className="mx-auto w-[90%]">
         <h2 className="text-4xl font-extrabold mb-4 text-black">Tech</h2>
-        <div className="grid grid-cols-2 md:grid-cols-8 gap-2">
+        <div className="grid grid-cols-1 md:grid-cols-8 gap-2">
           <div className="flex flex-col items-center">
-            <Image width={100} height={100} src="/html.svg" alt="HTML" />
+            <img className="md:w-[100px] w-[70px]" src="/html.svg" alt="HTML" />
           </div>
           <div className="flex flex-col items-center">
-            <Image width={100} height={100} src="/css.svg" alt="CSS" />
+            <img className="md:w-[100px] w-[70px]" src="/css.svg" alt="CSS" />
           </div>
           <div className="flex flex-col items-center">
-            <Image width={100} height={100} src="/js.svg" alt="JavaScript" />
+            <img
+              className="md:w-[100px] w-[70px]"
+              src="/js.svg"
+              alt="JavaScript"
+            />
           </div>
           <div className="flex flex-col items-center">
-            <Image width={100} height={100} src="/react.svg" alt="React" />
+            <img
+              className="md:w-[100px] w-[70px]"
+              src="/react.svg"
+              alt="React"
+            />
           </div>
           <div className="flex flex-col items-center">
-            <Image
-              width={100}
-              height={100}
+            <img
+              className="md:w-[100px] w-[70px]"
               src="/tailwind.svg"
               alt="Tailwind CSS"
             />
           </div>
           <div className="flex flex-col items-center">
-            <Image width={100} height={100} src="/ts.svg" alt="TypeScript" />
+            <img
+              className="md:w-[100px] w-[70px]"
+              src="/ts.svg"
+              alt="TypeScript"
+            />
           </div>
           <div className="flex flex-col items-center">
-            <Image width={100} height={100} src="/tail.svg" alt="Tail" />
+            <img className="md:w-[100px] w-[70px]" src="/tail.svg" alt="Tail" />
           </div>
           <div className="flex flex-col items-center">
-            <Image
-              width={100}
-              height={100}
+            <img
+              className="md:w-[100px] w-[70px]"
               src="/wordpress.svg"
               alt="WordPress"
             />
