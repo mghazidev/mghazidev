99343248d196101b98bc7e9898a6f8cb6f5b08c5
Muhammad Ghazi <mghazidev@gmail.com>
2026-02-23 23:38:07 +0500

    updated details

--- src/app/components/skills/Skills.tsx
+++ src/app/components/skills/Skills.tsx
@@ -113,6 +113,24 @@ const Skills = () => {
               alt="NodeJs"
             />
           </div>
+          <div className="flex flex-col items-center">
+            <Image
+              width={100}
+              height={100}
+              className="md:w-[100px] w-[70px]"
+              src="/c.svg"
+              alt="C++"
+            />
+          </div>
+          <div className="flex flex-col items-center">
+            <Image
+              width={100}
+              height={100}
+              className="md:w-[100px] w-[70px]"
+              src="/flutter-logo.svg"
+              alt="flutter"
+            />
+          </div>
 
           <div className="flex flex-col items-center">
             <Image
